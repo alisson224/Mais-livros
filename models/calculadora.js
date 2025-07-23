70d74943cad42c9666f5d3ebf89f07fe966227ab
alisson224 <celularredminote12ss@gmail.com>
2025-07-23 19:48:59 +0000

    terminei

--- models/calculadora.js
+++ models/calculadora.js
@@ -7,4 +7,4 @@ return n1 + n2
 }
 }
 
-module.exports = calculadora
+module.exports = calculadora 
---
+++ models/callback.js
@@ -0,0 +1,7 @@
+
+const fs = require ("node:fs")
+
+
+fs.readFile("./arq1", function ( error, resposta){
+  
+})

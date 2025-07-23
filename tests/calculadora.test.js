51eb4410fa0606cd797ae2848fe8048328717e26
alisson224 <celularredminote12ss@gmail.com>
2025-07-23 19:21:30 +0000

    terminei

---
+++ tests/calculadora.test.js
@@ -0,0 +1,17 @@
+//@ts-check
+
+const calculadora = require("../models/calculadora")
+
+test ("criando primeiro teste", function (){
+console.log ("oi");
+});
+
+test ("soma de 2 + 2 deve retornar 4", function (){
+const resultado = calculadora.soma (2,2)
+expect (resultado).toBe (4)
+});
+
+test ("soma de 2 + 4 deve retornar 6", function (){
+const resultado = calculadora.soma (2,4)
+expect (resultado).toBe (6)
+});

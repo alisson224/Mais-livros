5fc6cf2a71a5a4397fe4a69e516a0f51c11e8456
alisson224 <celularredminote12ss@gmail.com>
2025-08-20 17:28:08 +0000

    terminei

--- models/promise.js
+++ models/promise.js
@@ -1,9 +1,27 @@
 const readfile = require("node:readline");
+const { promiseHooks } = require("node:v8");
 const terminal = readfile.createInterface({
   input:process.stdin,
   output:process.stdout,
 });
 
-terminal.question("qual seu nome: \n",  function(valor) {
-   console.log("seja bem vindo " + valor);
+function questionpromisse(pergunta) {
+  const promessa = new Promise(function(resolve, reject){
+    terminal.question(pergunta, function (valor) {
+      resolve(valor);
+    });
+  });
+  return promessa;
+}
+
+question("qual seu nome ?\n", )
+   .then(function(nome) {
+    console.log("Nome:" + nome);
+ })
+
+.catch(function (error) {
+  console.log("Deu error" + error);
+})
+.finally(function () {
+  terminal.close();
 });

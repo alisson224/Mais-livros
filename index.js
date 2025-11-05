6b49f05fad3577ddb43d7e8f3da176b76c38e095
Álisson Souza, 3 Informática A - vespertino. <celularredminote12ss@gmail.com>
2025-11-05 16:47:25 +0000

    ok

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
+const http = require("node:http");
 const livroRepository = require("./infra/repository/livro");
 
-livroRepository
-.consultarTodos()
-.then(function (resultado) {
-  console.log(resultado);
-})
-.catch(function (error) {
-  console.log("Deu ruim", error);
+const server = http.createServer(function (req, res) {
+  //res.writeHead(200, {"content-type": "text/plain"})
+  res.end("Ok bem vindo");
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log("Inicializando servidor HTTP na porta " + port);
 });
